eccd22520ea932f3b93b014ed90792887bedb550
fvalverdeu <fvalverdeu@gmail.com>
2022-02-14 11:28:53 -0500

    update: add name and mac fields minew

--- api/models/minew.js
+++ api/models/minew.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const minewSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    mac: { type: String, required: true },
     description: { type: String, required: true },
     
 }, {versionKey: false });
