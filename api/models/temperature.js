5baef7d076d8b4847b3573f6f25e9849b78c2d35
fvalverdeu <fvalverdeu@gmail.com>
2022-04-08 02:16:57 -0500

    update temperature field date type

--- api/models/temperature.js
+++ api/models/temperature.js
@@ -4,7 +4,7 @@ const temperatureSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     preparation: { type: String, required: true },
     value: { type: String, required: true },
-    date: { type: String, required: true },
+    date: { type: Date, required: true },
     user: { type: String, required: true },
     observation: { type: String, required: true },
     place: { type: String, required: true },
