6d8e231e4f47fdfc6a7d4252c230966e7c64e8e9
fvalverdeu <fvalverdeu@gmail.com>
2022-04-22 02:01:57 -0500

    update: change type of date in calibration

--- api/models/calibration.js
+++ api/models/calibration.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const calibrationSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     value: { type: String, required: true },
-    date: { type: String, required: true },
+    date: { type: Date, required: true },
     user: { type: String, required: true },
 }, {versionKey: false });
 
